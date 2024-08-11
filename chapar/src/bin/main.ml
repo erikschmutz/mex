8ee42e900b87f90db23de37e35bc9372aa5ab64a
Erik Rehn <erik.rehn@altostruct.se>
2024-08-11 15:24:46 +0200

    fixes

--- chapar/src/bin/main.ml
+++ chapar/src/bin/main.ml
@@ -34,13 +34,7 @@ end
 
 
 
-(* open Utils.Common *)
-(* open Lib.Core.Runtime(Alg2)(Lib.) *)
 let () = 
-    (* if Array.length Sys.argv != 4 then 
-      raise (Invalid_argument ("Should be done in the following algorithm"));; *)
-
-    
     let protocol = Sys.argv.(1) in  
     let algorthim = Sys.argv.(2) in  
     let nid = int_of_string Sys.argv.(3) in   
@@ -48,40 +42,15 @@ let () =
     let port = 8060 + nid in 
     let bench =  Utils.Benchprog.prog_of_bench benchfile in 
     let config = Utils.ReadConfig.readConfiguration "config.txt" in  
-    (* TCPStartAlg1.start config 1 Skip *)
-    TCPStartAlg1.start_all (List.map (fun (id, conf) -> (
-      id, conf, if id != -1 then Utils.Common.Skip else bench
-    )) config)
-    
-    (* match (protocol, algorthim) with *)
-
-
-    
-     (* let protocol = Sys.argv.(1) in 
-     let algorthim = Sys.argv.(2) in  
+    let nodes = List.map (fun (id, conf) -> (
+      id, conf
+    )) config in 
+    let program =  if nid == -1 then Utils.Benchprog.prog_of_bench "bench.txt" else Skip in 
      match (protocol, algorthim) with
-        | ("tcp", "alg1") -> TCPStartAlg1.start(1, ({ port = 9000; ip = "0.0.0.0" }, Utils.Benchprog.prog_of_bench "bench.txt"));
-           
-         | ("tcp", "alg2") -> TCPStartAlg2.start([
-          (1, ({ port = 9000; ip = "0.0.0.0" }, Utils.Benchprog.prog_of_bench "bench.txt"));
-          (2, ({ port = 9001; ip = "0.0.0.0" }, Utils.Benchprog.prog_of_bench "bench.txt"))
-        ])
-        | ("tcp", "alg3") -> TCPStartAlg3.start([
-          (1, ({ port = 9000; ip = "0.0.0.0" }, Utils.Benchprog.prog_of_bench "bench.txt"));
-          (2, ({ port = 9001; ip = "0.0.0.0" }, Utils.Benchprog.prog_of_bench "bench.txt"))
-        ])
-        | ("udp", "alg1") -> UDPStartAlg1.start([
-          (1, ({ port = 9000; ip = "0.0.0.0" }, Utils.Benchprog.prog_of_bench "bench.txt"));
-          (2, ({ port = 9001; ip = "0.0.0.0" }, Utils.Benchprog.prog_of_bench "bench.txt"))
-        ])
-        | ("udp", "alg2") -> UDPStartAlg2.start([
-          (1, ({ port = 9000; ip = "0.0.0.0" }, Utils.Benchprog.prog_of_bench "bench.txt"));
-          (2, ({ port = 9001; ip = "0.0.0.0" }, Utils.Benchprog.prog_of_bench "bench.txt"))
-        ])
-        | ("udp", "alg3") -> UDPStartAlg3.start([
-          (1, ({ port = 9000; ip = "0.0.0.0" }, Utils.Benchprog.prog_of_bench "bench.txt"));
-          (2, ({ port = 9001; ip = "0.0.0.0" }, Utils.Benchprog.prog_of_bench "bench.txt"))
-        ])
-       
-  
-      else raise (Invalid_argument ("First argument needs to be either server or client found: \"" ^ arg ^"\""))  *)
+        | ("tcp", "alg1") -> TCPStartAlg1.start nodes nid program;
+        | ("tcp", "alg2") -> TCPStartAlg2.start nodes nid program;
+        | ("tcp", "alg3") -> TCPStartAlg3.start nodes nid program;   
+        | ("udp", "alg1") -> UDPStartAlg1.start nodes nid program;
+        | ("udp", "alg2") -> UDPStartAlg2.start nodes nid program;
+        | ("udp", "alg3") -> UDPStartAlg3.start nodes nid program;       
+        | _ -> raise (Invalid_argument ("Bad format args needs to be: \n chapar <protocol> <algorthim> <nid>"))
